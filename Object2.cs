d1d5138d4cccf835bcceec4e08c79fbbb1fb8bca
Sanjay304 <58076374+Sanjay304@users.noreply.github.com>
2019-11-28 16:53:44 +0530

    Examples of object
    How to initialize Object and how to Apply Objects in Code C#.

---
+++ Object2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Company
+{
+
+    String name;
+    String type;
+    int estrength;
+
+    public Company(String name, String type,
+                  int estrength)
+    {
+        this.name = name;
+        this.type = type;
+        this.estrength = estrength;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String gettype()
+    {
+        return type;
+    }
+
+    public int getestrength()
+    {
+        return estrength;
+   
+    }
+
+    public String toString()
+    {
+        return ("Hi my name is " + this.getName()
+                + ".\nMy type, estrength  are " + this.gettype()
+                + ", " + getestrength());
+    }
+
+    public static void Main(String[] args)
+    {
+
+        Company Phyelements = new Company("Phyelements", "Software", 20);
+        Console.WriteLine(Phyelements.toString());
+    }
+}
---
+++ Objects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Employee
+{
+    int id;  
+    String name;
+    String work;
+    String companyname;
+
+    public static void Main(string[] args)
+    {
+        Employee e1 = new Employee();   
+        e1.id = 77;
+        e1.name = "Sanjay Gowda";
+        e1.work = "Engineer";
+        e1.companyname = "PhyElements";
+        Console.WriteLine(e1.id);
+        Console.WriteLine(e1.name);
+        Console.WriteLine(e1.companyname);
+        Console.WriteLine(e1.work);
+
+    }
+}
