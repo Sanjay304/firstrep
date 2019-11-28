93a2aab1ba058dc2b17ee733dcaec7263592eb20
Sanjay304 <58076374+Sanjay304@users.noreply.github.com>
2019-11-28 17:15:26 +0530

    List collection C#
    Using Names in simple List collection in C#.

---
+++ List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListCollection
+{
+    public static void Main(string[] args)
+    {  
+        var names = new List<string>() { "Rajesh", "Spoorthi", "Harsha", "Sanjay","Sathis","Guru","Sachin", };
+
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
