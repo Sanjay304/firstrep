c106c9e86057c4b28f1f8fd6a2569be0d4ffe517
Sanjay304 <58076374+Sanjay304@users.noreply.github.com>
2019-11-28 13:56:08 +0530

    phyelements Employee name
    Create a object as  a integer and using even value display console.

---
+++ employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Phyelements
+{
+    public static void Main()
+    {
+        IList<int> intList = new List<int>() { 10, 20, 30, 40 };
+
+
+        Console.WriteLine(intList.Count);
+
+        IList<Employee> EmployeeList = new List<Employee>() {
+                new Employee(){ EmployeeID=2, EmployeeName="Rajesh"},
+                new Employee(){ EmployeeID=4, EmployeeName="Spoorthi"},
+                new Employee(){ EmployeeID=6, EmployeeName="Harsha"},
+                new Employee(){ EmployeeID=8, EmployeeName="Sanjay"}
+            };
+
+        Console.WriteLine(EmployeeList.Count);
+
+    }
+}
+
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string EmployeeName { get; set; }
+
+}
