8f845f9670b02659871bcfd6b191d6a190c7a631
Sanjay304 <58076374+Sanjay304@users.noreply.github.com>
2019-11-28 12:18:34 +0530

    program1
    fibanocci

---
+++ program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class FibonacciExample
+{
+    public static void Main(string[] args)
+    {
+        int n1 = 0, n2 = 1, n3, i, number;
+        Console.Write("Enter the Maximum value: ");
+        number = int.Parse(Console.ReadLine());
+        Console.Write(n1 + " " + n2 + " "); 
+        for (i = 2; i < number; ++i)    
+        {
+            n3 = n1 + n2;
+            if(i<number)
+            {
+            
+            }
+            Console.Write(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
