b9d7237959b9eedaf50600c87ffdae8eb43c8449
Sanjay304 <58076374+Sanjay304@users.noreply.github.com>
2019-11-28 18:17:12 +0530

    IEnumerable simple Example
    IEnumerable to Enamuretor Example in C#.

---
+++ IENumerable.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+public class Person
+{
+    public Person(string fName, string lName)
+    {
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+
+    public string firstName;
+    public string lastName;
+}
+public class People : IEnumerable
+{
+    private Person[] _people;
+    public People(Person[] pArray)
+    {
+        _people = new Person[pArray.Length];
+
+        for (int i = 0; i < pArray.Length; i++)
+        {
+            _people[i] = pArray[i];
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return (IEnumerator)GetEnumerator();
+    }
+
+    public PeopleEnum GetEnumerator()
+    {
+        return new PeopleEnum(_people);
+    }
+}
+public class PeopleEnum : IEnumerator
+{
+    public Person[] _people;
+    int position = -1;
+
+    public PeopleEnum(Person[] list)
+    {
+        _people = list;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return (position < _people.Length);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public Person Current
+    {
+        get
+        {
+            try
+            {
+                return _people[position];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
+
+class App
+{
+    static void Main()
+    {
+        Person[] peopleArray = new Person[3]
+        {
+            new Person("Sanjay", "Harsha"),
+            new Person("Guru", "Manju"),
+            new Person("Kishan", "Rahul"),
+        };
+
+        People peopleList = new People(peopleArray);
+        foreach (Person p in peopleList)
+            Console.WriteLine(p.firstName + " " + p.lastName);
+
+    }
+}
